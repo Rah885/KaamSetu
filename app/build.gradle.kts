a41dded056341766fc4fe7221edccdcf204dc764
Rah885 <desirahul272716@gmail.com>
2026-09-08 16:00:08 +0530

    Add navigation-compose dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,5 +51,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
+    implementation("androidx.navigation:navigation-compose:2.9.8")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
